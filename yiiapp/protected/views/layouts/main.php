a1fda859a6b0988ce65621862fcef0164200721d
Sergio Viera <sergioviera@gmail.com>
2018-08-30 10:18:39 -0300

    agregar jquery

--- yiiapp/protected/views/layouts/main.php
+++ yiiapp/protected/views/layouts/main.php
@@ -12,6 +12,18 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection">
 	<![endif]-->
 
+	<!-- jQuery Rocks! \m/ -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css">
 
@@ -30,7 +42,14 @@
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'Administracion', 'url'=>array('/site/page', 'view'=>'about')),
+
+				array('label'=>'Asignaturas', 'url'=>array('/asignaturas/index', 'view'=>'about')),
+				array('label'=>'Carreras', 'url'=>array('/carreras/index', 'view'=>'about')),
+				array('label'=>'Asignaturas-Profesor', 'url'=>array('/asignaturaprofesor/index', 'view'=>'about')),
+				array('label'=>'Inscripciones', 'url'=>array('/incripciones/index', 'view'=>'about')),
+				array('label'=>'Alumnos', 'url'=>array('/participants/index', 'view'=>'about')),
+				array('label'=>'Profesores', 'url'=>array('/profesores/index', 'view'=>'about')),
+
 				array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
