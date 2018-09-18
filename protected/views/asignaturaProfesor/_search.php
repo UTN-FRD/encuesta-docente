96673617db90179ee2af05e9b090652960882bc1
DAMoreira <sk4rfac3@gmail.com>
2018-09-18 20:49:50 -0300

    #15 busqueda avanzada con lista desplegable

--- protected/views/asignaturaProfesor/_search.php
+++ protected/views/asignaturaProfesor/_search.php
@@ -11,23 +11,22 @@
 	'method'=>'get',
 )); ?>
 
-	<div class="row">
-		<?php echo $form->label($model,'asignatura_id'); ?>
-		<?php echo $form->textField($model,'asignatura_id'); ?>
+	<div>
+		<?php echo $form->labelEx($model,'asignatura_id'); ?>
+		<?php echo $form->dropDownList($model,'asignatura_id',CHtml::listData(Asignaturas::model()->findall(),"id","descripcion","plan"),array('empty'=>'Todos')); ?>
+		<!-- <?php echo $form->textField($model,'asignatura_id'); ?> -->
+		<?php echo $form->error($model,'asignatura_id'); ?>
 	</div>
 
-	<div class="row">
-		<?php echo $form->label($model,'profesor_id'); ?>
-		<?php echo $form->textField($model,'profesor_id'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'id'); ?>
-		<?php echo $form->textField($model,'id'); ?>
+	<div>
+		<?php echo $form->labelEx($model,'profesor_id'); ?>
+		<?php echo $form->dropDownList($model,'profesor_id',CHtml::listData(Profesores::model()->findall(),"id","nombre"),array('empty'=>'Todos')); ?>
+		<!-- <?php echo $form->textField($model,'profesor_id'); ?> -->
+		<?php echo $form->error($model,'profesor_id'); ?>
 	</div>
 
 	<div class="row buttons">
-		<?php echo CHtml::submitButton('Search'); ?>
+		<?php echo CHtml::submitButton('Search',array("class"=>"btn btn-primary btn-large")); ?>
 	</div>
 
 <?php $this->endWidget(); ?>
