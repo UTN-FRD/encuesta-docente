8723c65d9ab7b09feffa83d63fe16fc2d646d768
Sergio Viera <sergioviera@gmail.com>
2018-09-17 21:37:35 -0300

    ajustar pantalla de encuesta

--- yiiapp/protected/views/incripciones/encuestas.php
+++ yiiapp/protected/views/incripciones/encuestas.php
@@ -4,13 +4,14 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+<h2>Encuestas pendientes de completar</h2>
 
+<div>
 <?php
 $usuario = Yii::app()->user->name;
 $array = array('A','B','C','D','E','F');
 foreach($array as $elemento){ ?>
-    <p align="center"><tr>
-        <?php echo CHtml::button('Encuesta '.$elemento.' de '.$usuario,array("class"=>"btn btn-primary btn-large")); ?>
-    </tr></p>
+        <?php echo CHtml::button('Encuesta '.$elemento.' de '.$usuario,array("class"=>"btn btn-primary btn-large", "style"=>"width:100%;margin:5px")); ?>
 <?php } ?>
+
+</div>
