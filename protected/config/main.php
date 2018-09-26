c5b93494892bac5ef0e156f3d7109757d387da49
DAMoreira <sk4rfac3@gmail.com>
2018-09-26 17:10:18 -0300

    xd

--- protected/config/main.php
+++ protected/config/main.php
@@ -17,19 +17,19 @@ return array(
 		'application.models.*',
 		'application.components.*',
 	),
-	
+	/*
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
 		
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'julianpelotudo',
+			'password'=>'password',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
 		
 	),
-	
+	*/
 	// application components
 	'components'=>array(
 
