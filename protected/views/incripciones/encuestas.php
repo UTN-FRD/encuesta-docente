b8a2501a3eae27fd636bc660867a97d741f0b215
DAMoreira <sk4rfac3@gmail.com>
2018-09-26 18:07:19 -0300

    Tokens individuales por asignatura profesor

--- protected/views/incripciones/encuestas.php
+++ protected/views/incripciones/encuestas.php
@@ -22,12 +22,6 @@ foreach(array_column($idUsuario,'participant_id') as $idTemp)
 $idAsignaturas = array_column($asignaturas,'asignatura_id');
 $asignaturaProfesor = AsignaturaProfesor::model()->findAllByAttributes(array('asignatura_id'=>$idAsignaturas));
 $token=min(array_column($idUsuario,'participant_id'));
-$token = str_replace(array('0','1','2','3','4','5','6','7','8','9'),array('a','b','c','d','e','f','g','h','i','j'), $token);
-if (Tokens::model()->findAllByAttributes(array('token'=>$token))===array()) {
-        $newToken=new Tokens;
-        $newToken->token=$token;
-        $newToken->save();
-}
 foreach($asignaturaProfesor as $elemento){ ?>
         <?php
         $asignatura = implode(array_column(array($elemento),'asignatura_id'));
@@ -38,11 +32,18 @@ foreach($asignaturaProfesor as $elemento){ ?>
         $asignatura = Asignaturas::model()->findAllByAttributes(array('id'=>$asignatura));
         $asignatura = array_column($asignatura,'descripcion');
         $asignatura = implode($asignatura);
+        $asignaturaProfesorId = $elemento->id;
+        $token = sha1($asignaturaProfesorId.$usuario);
+        if (Tokens::model()->findAllByAttributes(array('token'=>$token))===array()) {
+                $newToken=new Tokens;
+                $newToken->token=$token;
+                $newToken->save();
+        }
         echo CHtml::button(
                 'Materia: '.$asignatura.' de: '.$profesor,
                 array(
                         'class'=>"btn btn-primary btn-large", "style"=>"width:100%; height:100%; margin: 5px; white-space: normal",
-                        'onclick'=>"window.open(`//localhost/encuesta-docente/limesurvey/index.php/164846?token={$token}`)",
+                        'onclick'=>"window.open(`//localhost/encuesta-docente/limesurvey/index.php/164846?token={$token}&asignatura_profesor_id={$asignaturaProfesorId}`)",
                 )
         );
         ?>
