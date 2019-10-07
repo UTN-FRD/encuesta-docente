4ed2c450f320dc9e2cb45d7ae8c32f5c98d7a328
nulmete <nicoulmete1@gmail.com>
2019-10-07 19:28:01 -0300

    #23 Gráfico #inscriptos por carrera

--- protected/views/incripciones/admin.php
+++ protected/views/incripciones/admin.php
@@ -29,6 +29,40 @@ $inscripciones = $_GET["Incripciones"] ?? '';
 
 <h1>Inscripciones <span name="year"><?= $inscripciones["anio_academico"] ?? '' ?></span></h1> 
 
+<?php
+	function crearDatos() {
+		$inscripcionesSQL = array();
+		$carrerasSQL = array();
+		$pares = array();
+
+		$list = Yii::app()->db->createCommand('
+			SELECT c.description as CARRERA, 
+			count(1) as INSCRIPCIONES FROM incripciones i
+			INNER JOIN asignaturas a
+			ON i.asignatura_id = a.id 
+			INNER JOIN carreras c
+			ON a.carrera_id = c.id
+			WHERE i.anio_academico = 2018
+			GROUP BY c.description
+		')->queryAll();
+
+		$tituloYDato = array_column($list, 'INSCRIPCIONES', 'CARRERA');
+
+		$arrayDatos = array();
+		array_push($arrayDatos, array('Inscripciones', 'Carreras'));
+
+		foreach ($tituloYDato as $key => $value) {
+			array_push($arrayDatos, array($key, (int) $value));
+		}
+
+		return $arrayDatos;
+	}
+	
+	$grafico = $this->widget('ext.Hzl.google.HzlVisualizationChart', array('visualization' => 'PieChart',
+		'data' => crearDatos(),
+		'options' => array('title' => 'Inscripciones por carrera')));
+?>
+
 <?php echo CHtml::link('Búsqueda Avanzada','#',array('class'=>'search-button')); ?>
 <div class="search-form" style="display:none">
 <?php $this->renderPartial('_search',array(
