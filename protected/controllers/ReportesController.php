a50946a35ac69674110d060c95b9e94a18806db8
Sergio Viera <sergioviera@gmail.com>
2019-10-09 10:24:33 -0300

    mover logica dase de datos al controlador

--- protected/controllers/ReportesController.php
+++ protected/controllers/ReportesController.php
@@ -14,7 +14,31 @@ class ReportesController extends Controller
 	 */
 	public function actionIndex()
 	{
-		$this->render('index');
+		$this->render('index',array(
+			'inscripcionesPorCarrera'=> $this->loadInscripcionesPorCarrera()
+		));
 	}
+
+	public function loadInscripcionesPorCarrera() {
+		$list = Yii::app()->db->createCommand('
+			SELECT c.description as CARRERA, count(1) as INSCRIPCIONES 
+			FROM incripciones i INNER JOIN asignaturas a ON i.asignatura_id = a.id 
+			    INNER JOIN carreras c ON a.carrera_id = c.id
+			WHERE i.anio_academico = 2019
+			GROUP BY c.description
+		')->queryAll();
+
+		$tituloYDato = array_column($list, 'INSCRIPCIONES', 'CARRERA');
+
+		$arrayDatos = array();
+		array_push($arrayDatos, array('Inscripciones', 'Carreras'));
+
+		foreach ($tituloYDato as $key => $value) {
+			array_push($arrayDatos, array($key, (int) $value));
+		}
+
+		return $arrayDatos;
+	}
+
 
 }
--- protected/views/reportes/index.php
+++ protected/views/reportes/index.php
@@ -6,42 +6,16 @@ $this->breadcrumbs=array(
 	'Reportes',
 );
 
-$this->menu=array(
-);
+$this->menu=array();
 ?>
 
 <h1>Reportes</h1>
 
 <?php
-	function crearDatos() {
-		$inscripcionesSQL = array();
-		$carrerasSQL = array();
-		$pares = array();
-
-		$list = Yii::app()->db->createCommand('
-			SELECT c.description as CARRERA, 
-			count(1) as INSCRIPCIONES FROM incripciones i
-			INNER JOIN asignaturas a
-			ON i.asignatura_id = a.id 
-			INNER JOIN carreras c
-			ON a.carrera_id = c.id
-			WHERE i.anio_academico = 2018
-			GROUP BY c.description
-		')->queryAll();
-
-		$tituloYDato = array_column($list, 'INSCRIPCIONES', 'CARRERA');
-
-		$arrayDatos = array();
-		array_push($arrayDatos, array('Inscripciones', 'Carreras'));
-
-		foreach ($tituloYDato as $key => $value) {
-			array_push($arrayDatos, array($key, (int) $value));
-		}
-
-		return $arrayDatos;
-	}
 	
-	$grafico = $this->widget('ext.Hzl.google.HzlVisualizationChart', array('visualization' => 'PieChart',
-		'data' => crearDatos(),
-		'options' => array('title' => 'Inscripciones por carrera')));
+	$grafico = $this->widget('ext.Hzl.google.HzlVisualizationChart', 
+		array(
+			'visualization' => 'PieChart',
+			'data' => $inscripcionesPorCarrera,
+			'options' => array('title' => 'Inscripciones por carrera')));
 ?>
