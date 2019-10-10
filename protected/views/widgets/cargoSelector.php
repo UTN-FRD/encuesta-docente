fd9da2bc685105b03d6d3ea5070a62f61694b064
nulmete <nicoulmete1@gmail.com>
2019-10-10 15:28:26 -0300

    #26 Agregar cargo Laboratorio

--- protected/views/widgets/cargoSelector.php
+++ protected/views/widgets/cargoSelector.php
@@ -2,7 +2,11 @@
 	<?php echo $form->labelEx($model,'cargo', array('style'=>'text-align:left')); ?>
 	<?php echo $form->dropDownList(
 			$model,'cargo',
-			array('Titular'=>'Titular','Auxiliar'=>'Auxiliar'),
+			array(
+				'Titular'=>'Titular',
+				'Auxiliar'=>'Auxiliar',
+				'Laboratorio'=>'Laboratorio'
+			),
 			array('empty'=>'Seleccione','class'=>'form-control')
 			); ?>
 	<?php echo $form->error($model,'cargo'); ?>
