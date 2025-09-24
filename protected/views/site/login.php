580389776759929e28095db0398f4a2ebacb28fd
Sergio Viera <sergioviera@gmail.com>
2025-09-24 09:47:11 -0300

    eliminar videos

--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -17,19 +17,6 @@ $this->pageTitle=Yii::app()->name . ' - Login';
 //	'Login',
 //);
 ?>
-<button type="button" class="btn btn-danger pull-right" data-toggle="modal" data-target="#helpModal">?</button>
-<div class="modal fade" id="helpModal" tabindex="-1" role="dialog" aria-labelledby="helpModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-body">
-		<video width="100%" height="500" controls>
-		  <source src="/login.mp4" type="video/mp4">
-		Your browser does not support the video tag.
-		</video>
-      </div>
-    </div>
-  </div>
-</div>
 <h1>
 	UTN - FRD <br>
 	Sistema de Encuestas Estudiantes
