e4acaf75651594f7889c9cc2cd26e292b3c861a2
Sergio Viera <sergioviera@gmail.com>
2025-09-23 21:45:29 -0300

    dockerize! and ignore files

--- protected/config/database.php
+++ protected/config/database.php
@@ -1,12 +1,9 @@
 <?php
 
-// This is the database connection configuration.
 return array(
-	
-	'connectionString' => 'mysql:host=127.0.0.1;dbname=limesurvey',
+	'connectionString' => 'mysql:host=db;dbname=limesurvey',
 	'emulatePrepare' => true,
 	'username' => 'root',
-	'password' => '',
+	'password' => 'root',
 	'charset' => 'utf8',
-
 );
